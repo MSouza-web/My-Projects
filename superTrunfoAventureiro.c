3bb8925648b64f4714c76e34a6365ee9ebef35b6
MSouza-web <graduacao2marcos@gmail.com>
2025-05-26 18:34:02 +0000

    commit3

--- superTrunfoAventureiro.c
+++ superTrunfoAventureiro.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int main(){
-    
+    int main() {
 
+    char estado1, estado2;
+    char codigo1[5], codigo2[5];
+    char cidade1[50], cidade2[50];
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontos1, pontos2;
+    float densidade1, densidade2;
+    float pib_per_capita1, pib_per_capita2;
+
+ 
     return 0;
 }
