cf285a6a9413ea65e0c88a33b9e737cab257a639
MSouza-web <graduacao2marcos@gmail.com>
2025-05-26 19:08:17 +0000

    commit5

--- superTrunfoAventureiro.c
+++ superTrunfoAventureiro.c
@@ -9,8 +9,36 @@
     float area1, area2;
     float pib1, pib2;
     int pontos1, pontos2;
+
     float densidade1, densidade2;
     float pib_per_capita1, pib_per_capita2;
+
+    printf("=== Carta 01 ===\n");
+
+    printf("Digite o estado: ");
+    scanf(" %c", &estado1);
+
+    printf("Digite o código: ");
+    scanf("%s", &codigo1);
+
+    printf("Digite a cidade: ");
+    scanf("%s", &cidade1);
+
+    printf("Digite a população: ");
+    scanf("%d", &populacao1);
+
+    printf("Digite a área em km²: ");
+    scanf("%f", &area1);
+
+    printf("Digite o PIB: ");
+    scanf("%f", &pib1);
+
+    printf("Total de pontos turísticos: ");
+    scanf("%d", &pontos1);
+
+    
+    densidade1 = populacao1 / area1;
+    pib_per_capita1 = pib1 / populacao1;
 
  
     return 0;
